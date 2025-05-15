ec3ab83f9e879ef3873b535423c7438a97359f25
itscrystalline <pvpthadgaming@gmail.com>
2025-05-15 22:53:52 +0700

    idk

--- pkgs/ha_tuya_ble/default.nix
+++ pkgs/ha_tuya_ble/default.nix
@@ -3,29 +3,31 @@
   lib,
   buildHomeAssistantComponent,
   fetchFromGitHub,
-}:
-buildHomeAssistantComponent rec {
-  owner = "jbsky";
-  domain = "tuya_ble";
-  version = "0.2.3";
+}: let
+  inherit (pkgs.home-assistant.python.pkgs) tuya-iot-py-sdk pycountry;
+in
+  buildHomeAssistantComponent rec {
+    owner = "jbsky";
+    domain = "tuya_ble";
+    version = "0.2.3";
 
-  src = fetchFromGitHub {
-    inherit owner;
-    repo = "ha_tuya_ble";
-    rev = version;
-    hash = "sha256-7NMiZC/8ax2TUsxYOykKtg+vOvzMvcolqBi15MfC8w0=";
-  };
+    src = fetchFromGitHub {
+      inherit owner;
+      repo = "ha_tuya_ble";
+      rev = version;
+      hash = "sha256-7NMiZC/8ax2TUsxYOykKtg+vOvzMvcolqBi15MfC8w0=";
+    };
 
-  dependencies = with pkgs.python312Packages; [
-    tuya-iot-py-sdk
-    pycountry
-  ];
+    dependencies = [
+      tuya-iot-py-sdk
+      pycountry
+    ];
 
-  meta = with lib; {
-    changelog = "https://github.com/jbsky/ha_tuya_ble/releases/tag/${version}";
-    description = "Home Assistant support for Tuya BLE devices ";
-    homepage = "https://github.com/jbsky/ha_tuya_ble";
-    maintainers = [];
-    license = licenses.mit;
-  };
-}
+    meta = with lib; {
+      changelog = "https://github.com/jbsky/ha_tuya_ble/releases/tag/${version}";
+      description = "Home Assistant support for Tuya BLE devices ";
+      homepage = "https://github.com/jbsky/ha_tuya_ble";
+      maintainers = [];
+      license = licenses.mit;
+    };
+  }
